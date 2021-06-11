a614bb2fb892e4e4b9693e4e268945c71186367c
Zulfikarrish <fikararis35@gmail.com>
2021-06-11 19:08:03 +0700

    Tugas CRUD Selesai

---
+++ README.md
@@ -0,0 +1,7 @@
+Nama : Muhammad Zulfikar Aris
+
+NIM : D121191064
+
+Mata Kuliah : Pemrograman Aplikasi Mobile b
+
+Tugas CRUD Pemrograman Aplikasi Mobile B
---
+++ app/build/generated/source/buildConfig/debug/com/Final_D121191064/CRUDbiodata/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.Final_D121191064.CRUDbiodata;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.Final_D121191064.CRUDbiodata";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
